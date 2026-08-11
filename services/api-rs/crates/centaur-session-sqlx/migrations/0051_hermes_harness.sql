a1b260a808552f288cfe59d149455d4cd53a1c98
Matthew Slipper <me@matthewslipper.com>
2026-08-11 09:07:19 -0600

    refactor: split Hermes database migration

--- services/api-rs/crates/centaur-session-sqlx/migrations/0051_hermes_harness.sql
+++
@@ -1,6 +0,0 @@
-alter table sessions
-drop constraint sessions_harness_type_supported;
-
-alter table sessions
-add constraint sessions_harness_type_supported
-check (harness_type in ('codex', 'amp', 'claudecode', 'nanocodex', 'hermes'));
